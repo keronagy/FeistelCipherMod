ab599a365ae22312cf2e4c18abb45e887116f64a
Kyrillos Nagy <kyrillos.n.wadie@gmail.com>
2018-08-03 13:46:38 +0200

    F function modification

--- src/feistelciphermod/FeistelCipherMod.java
+++ src/feistelciphermod/FeistelCipherMod.java
@@ -17,7 +17,8 @@ public class FeistelCipherMod {
     public static void main(String[] args) {
         // TODO code application logic here
         fFunction f = new fFunction();
-        f.run();
+        f.run(98036585L,260080082649971L);
+        
     }
     
 }
